5875e5001fdd57372a4f00dde4987e18bec34989
Aaron Nazareth <aaronnazareth@hotmail.co.uk>
2022-05-06 21:00:29 +0100

    Convert number to string and find smallest integer exercises done

---
+++ src/main/scala/ConvertNumberToString.scala
@@ -0,0 +1,7 @@
+// Create a function that can transform a number into a string.
+
+object ConvertNumberToString {
+
+  def numberToString(n: Int): String = n.toString
+
+}
--- src/main/scala/EvenOrOdd.scala
+++ src/main/scala/EvenOrOdd.scala
@@ -1,3 +1,5 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
 object EvenOrOdd extends App {
 
   def evenOrOdd(number: Int): String = {
